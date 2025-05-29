27fbef8876a5c0523f94fbcd678b0edd259011d2
veekay001 <kiki.ngarka@gmail.com>
2025-05-29 13:25:09 +0100

    updated interest file:corrected variable names

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,8 +1,8 @@
 
-P = 1000 #initial investment
-R = 0.005 #annual rate
-T = 3 #time in years
+principal = 1000 #initial investment
+rate = 0.005 #annual rate
+time = 3 #time in years
 
-interest = P * R * T
+interest = principal * rate * time
 
 print("the simple interest is : ", interest)
