49b4de7c46bdc5b534b3cfff5f15c2075f16bb6f
veekay001 <kiki.ngarka@gmail.com>
2025-06-01 21:52:33 +0100

    created daily reminder file

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,6 +1,6 @@
 task = input("Enter your Task: ")
-priority = input("Priority (high, medium, low): ").lower()
-time_bound = input("Is it time-bound? (yes/no): ").lower()
+priority = input("Priority (high, medium, low): ")
+time_bound = input("Is it time-bound? (yes/no): ")
 
 
 reminder = f"Task: '{task}' is marked as '{priority}' priority."
