801cdfd645185ff893932516f33d310c4e068992
veekay001 <kiki.ngarka@gmail.com>
2025-06-08 17:17:02 +0100

    created arithmetic operations module

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,12 +1,12 @@
-def perform_operation(num1: float, num2: float, operation :str):
+def perform_operation(num1,num2,operation):
      
-     if operation == 'add':
+     if operation == "add":
         return num1 + num2
-     elif operation == 'subtract':
+     elif operation == "subtract":
         return num1 - num2
-     elif operation == 'multiply':
+     elif operation == "multiply":
         return num1 * num2
-     elif operation == 'divide':
+     elif operation == "divide":
         if num2 != 0:
             return num1 / num2
         else:
